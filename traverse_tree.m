65d015f144c9f957a1d0b867ad9e6162bea93e02
dongmingsun <dongming.sun@outlook.com>
2018-02-05 22:15:16 +0000

    generate the new tree

--- traverse_tree.m
+++ traverse_tree.m
@@ -4,9 +4,21 @@ global tree_nodes;
 tree_nodes = {{0,tree}};
 traverse_tree_sub(tree,1);
 node_number = length(tree_nodes)
-nodes_to_plot = zeros(1,node_number);
+nodes_to_plot = zeros(3,node_number);
 for i = 1:node_number
-	nodes_to_plot(i)=tree_nodes{i}{1};
+	nodes_to_plot(1,i)=tree_nodes{i}{1};
+	if isempty(tree_nodes{i}{2}.op)
+		nodes_to_plot(2,i)=nan;
+	else
+		nodes_to_plot(2,i)=tree_nodes{i}{2}.op;
+	end
+
+	if isempty(tree_nodes{i}{2}.class)
+		nodes_to_plot(3,i)=nan;
+	else
+		nodes_to_plot(3,i)=tree_nodes{i}{2}.class;
+	end
+
 end
 end
 
--- visualise.m
+++ visualise.m
@@ -1,9 +1,12 @@
 function visualise(tree)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+fig_tree = figure(1);
+clf(fig_tree)
 global tree_nodes;
 nodes_to_plot = traverse_tree(tree);
-[x,y] = treelayout(nodes_to_plot);
+nodes_for_treeplot = nodes_to_plot(1,:);
+[x,y] = treelayout(nodes_for_treeplot);
 leaves = find( y == min(y) );
 num_layers = 1/min(y)-1;
 chains = zeros(num_layers, length(leaves));
@@ -11,40 +14,48 @@ for l=1:length(leaves)
     index = leaves(l);
     chain = [];
     chain(1) = index;
-    parent_index = nodes_to_plot(index);
+    parent_index = nodes_for_treeplot(index);
     j = 2;
     while (parent_index ~= 0)
         chain(j) = parent_index;
-        parent_index = nodes_to_plot(parent_index);
+        parent_index = nodes_for_treeplot(parent_index);
         j = j+1;
     end
     chains(:,l) = padarray(flip(chain), [0, num_layers-length(chain)], 'post');
 end
-% clf()
-figure(1)
+
 y_new = zeros(size(y));
-for i=1:length(nodes_to_plot)
+for i=1:length(nodes_for_treeplot)
     [r,c] = find(chains==i, 1);
     y_new(i) = max(y) - (r-1)*1/(num_layers+1);
 end
 
-plot(x, y_new, 'o');
-hold on
+
 for c=1:size(chains, 2)
     line_x = x(chains(chains(:,c)>0, c));
     line_y = y_new(chains(chains(:,c)>0, c));
-    line(line_x, line_y);
+    line(line_x, line_y,'linewidth',1);
 end
+hold on
+leaf_nodes_0 = find(nodes_to_plot(3,:)==0); 
+leaf_nodes_1 = find(nodes_to_plot(3,:)==1); 
+internal_nodes = find(~isnan(nodes_to_plot(2,:))); 
+% plot(x(internal_nodes), y_new(internal_nodes), 'ws','MarkerFaceColor','w');
+% hold on
+plot(x(leaf_nodes_0), y_new(leaf_nodes_0), 'o','color',[0.961, 0.263, 0.212],'MarkerFaceColor',[0.961, 0.263, 0.212]);
+hold on
+plot(x(leaf_nodes_1), y_new(leaf_nodes_1), 'o','color',[0.000, 0.588, 0.529],'MarkerFaceColor',[0.000, 0.588, 0.529]);
 
-for t=1:length(nodes_to_plot)
-    if isempty(tree_nodes{t}{2}.op)
-        node_text = num2str(tree_nodes{t}{2}.class);
-    else
-        node_text = "AU\_"+ num2str(tree_nodes{t}{2}.op(1));
+
+for t=1:length(nodes_for_treeplot)
+    if isempty(tree_nodes{t}{2}.class)
+        node_text = num2str(tree_nodes{t}{2}.op(1));
+        text(x(t), y_new(t), node_text,'FontWeight','bold', 'fontsize',12);  
+        % text(x(t), y_new(t), node_text,'FontWeight','bold', 'fontsize',12,'fontname','Roboto');  
     end
-    text(x(t)+0.008, y_new(t), node_text); % 
 end
-xlim([0 1]);
-ylim([0 1]);
+% xlim([0 1]);
+% ylim([0 1]);
+axis off
 end
 
